d4e3a7420e4c8290e805375a0b55cc9107ba4c05
dzowa <dzowa@DESKTOP-3BFB78V>
2025-11-30 00:26:51 +0100

    Add books Search draft logic

---
+++ OpenLibraryApi/BooksSearchExecutor.cs
@@ -0,0 +1,36 @@
+﻿using OpenLibraryApi.BooksSearches;
+using OpenLibraryApiConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLibraryApi
+{
+    public class BooksSearchExecutor<T> where T: IBooksSearch
+    {
+        private IBooksSearch _booksSearch { get; set; }
+        private ApiConnection _apiConnection { get; set; }
+        private Dictionary<BooksSearchType, T> _searchFunctionDictionary { get; set; }
+
+        public BooksSearchExecutor()
+        {
+            var item = new BooksByAuthorSearch();
+            _searchFunctionDictionary = new Dictionary<BooksSearchType, T>()
+            {
+              
+            };
+        }
+
+        //public void ExecuteSearch(BooksSearchType booksSearchType)
+        //{
+        //
+        //}
+        //
+        //private IBooksSearch ReturnProperSearchType(BooksSearchType booksSearchType)
+        //{
+        //
+        //}
+    }
+}
